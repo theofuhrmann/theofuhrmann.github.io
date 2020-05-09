a3a36696dc21ab192f956bcd708935c5e0bf6146
[Théo Fuhrmann] <theositges@gmail.com>
2020-05-10 00:29:52 +0200

    New palette

--- sideBar/sideBar.js
+++ sideBar/sideBar.js
@@ -1,36 +1,34 @@
+let xoff = 0.0;
+
 function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent('sketch');
+  createCanvas(400, windowHeight);
 }
 
 function draw() {
   blendMode(BLEND);
   background(255);
-  //noStroke();
-  blendMode(MULTIPLY);
   noStroke();
-  translate(-0.9*width/2,0.9*height/2);
-  fill(0,255,255);
-  drawLiq(19,35,23,10000);
-  fill(255,255,0);
-  drawLiq(15,45,20,20000);
-  fill(255,0,255);
-  drawLiq(17,25,17,50000);
+  blendMode(MULTIPLY);
+  fill(253, 253, 149);
+  wave(250,375, 0.05);
+  fill(104, 145, 195);
+  wave(250,375, 0.03);
+  fill(246, 146, 188);
+  wave(250,375, 0.07);
 }
 
-
-function drawLiq(vNnum, nm, sm, fcm){
-  push();
-  rotate(frameCount/(fcm*1000));
-  let dr = TWO_PI/vNnum;
+function wave(x0, x1, t) {
   beginShape();
-  for(let i = 0; i  < vNnum + 3; i++){
-    let ind = i%vNnum;
-    let rad = dr *ind;
-    let r = height + noise(frameCount/nm/50 + ind) * height*0.085 + sin(frameCount/sm + ind)*height*0.025;
-    r *= 1.25;
-    curveVertex(cos(rad)*r, sin(rad)*r);
+  let yoff = 0;
+  
+  for (let y = 0; y <= height; y += 15) {
+    let x = map(noise(yoff, xoff), 0, 1, x0, x1);
+    vertex(x, y);
+    yoff += t;
   }
-  endShape();
-  pop();
+  xoff += 0.002;
+  vertex(400, height);
+  vertex(0, height);
+  vertex(0,0);
+  endShape(CLOSE);
 }
